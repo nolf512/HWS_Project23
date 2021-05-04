ae1e246ddcb70d9644e72fb9842faed53bd942d8
nofprog <k.jin3217@gmail.com>
2021-05-04 21:27:33 +0900

    createScore(), createLives(), createSlices()

--- HWS_Project23/GameScene.swift
+++ HWS_Project23/GameScene.swift
@@ -9,25 +9,75 @@ import SpriteKit
 
 class GameScene: SKScene {
     
-    var scoreLabel: SKLabelNode!
+    var gameScore: SKLabelNode!
     
     var livesImages = [SKSpriteNode]()
     var lives = 3
     
     var score = 0 {
         didSet {
-            scoreLabel.text = "score: \(score)"
+            gameScore.text = "score: \(score)"
         }
     }
 
+    var activeSliceBG: SKShapeNode!
+    var activeSliceFG: SKShapeNode!
+    
     override func didMove(to view: SKView) {
         
+        //背景
         let background = SKSpriteNode(imageNamed: "background")
         background.position = CGPoint(x: 512, y: 384)
         background.blendMode = .replace
         background.zPosition = -1
         addChild(background)
         
+        //物理関連
+        physicsWorld.gravity = CGVector(dx: 0, dy: -6)
+        physicsWorld.speed = 0.85
+        
+        createScore()
+        createLives()
+        createSlices()
+    }
+    
+    
+    func createScore(){
+        gameScore = SKLabelNode(fontNamed: "Chalkduster")
+        gameScore.horizontalAlignmentMode = .left
+        gameScore.fontSize = 48
+        addChild(gameScore)
+        
+        gameScore.position = CGPoint(x: 8, y: 8)
+        score = 0
+    }
+    
+    func createLives(){
+        //sliceLifeイメージを３つ配置
+        for i in 0 ..< 3 {
+            let spriteNode = SKSpriteNode(imageNamed: "sliceLife")
+            spriteNode.position = CGPoint(x: CGFloat(834 + (i * 70)), y: 720)
+            addChild(spriteNode)
+            
+            livesImages.append(spriteNode)
+        }
+    }
+    
+    func createSlices(){
+        activeSliceBG = SKShapeNode()
+        activeSliceBG.zPosition = 2
+        
+        activeSliceFG = SKShapeNode()
+        activeSliceFG.zPosition = 3
+        
+        activeSliceBG.strokeColor = UIColor(red: 1, green: 0.9, blue: 0, alpha: 1)
+        activeSliceBG.lineWidth = 9
+        
+        activeSliceFG.strokeColor = UIColor.white
+        activeSliceFG.lineWidth = 5
+        
+        addChild(activeSliceBG)
+        addChild(activeSliceFG)
     }
     
 }
