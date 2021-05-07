77932ca78016ab2cd343829bdb2c7a1dafe39c66
nofprog <k.jin3217@gmail.com>
2021-05-07 23:59:53 +0900

    touchesMoved追記

--- HWS_Project23/GameScene.swift
+++ HWS_Project23/GameScene.swift
@@ -127,6 +127,64 @@ class GameScene: SKScene {
             playSwooshSound()
         }
         
+        let nodesAtPoint = nodes(at: location)
+        
+        for case let node as SKSpriteNode in nodesAtPoint {
+            if node.name == "enemy" {
+                
+                //destroy penguin
+                if let emitter = SKEmitterNode(fileNamed: "sliceHitEnemy") {
+                    emitter.position = node.position
+                    addChild(emitter)
+                    
+                    node.name = ""
+                    
+                    node.physicsBody?.isDynamic = false
+                    
+                    let scaleOut = SKAction.scale(to: 0.001, duration: 0.2)
+                    let fadeOut = SKAction.fadeOut(withDuration: 0.2)
+                    let group = SKAction.group([scaleOut, fadeOut])
+                    
+                    let seq = SKAction.sequence([group, .removeFromParent()])
+                    node.run(seq)
+                    
+                    score += 1
+                    
+                    if let index = activeEnemies.firstIndex(of: node) {
+                        activeEnemies.remove(at: index)
+                    }
+                    run(SKAction.playSoundFileNamed("whack.caf", waitForCompletion: false))
+                }
+                
+            } else if node.name == "bomb" {
+                //destroy bomb
+                
+                guard let bombContainer = node.parent as? SKSpriteNode else { continue }
+                
+                if let emitter = SKEmitterNode(fileNamed: "sliceHitBomb") {
+                    emitter.position = bombContainer.position
+                    addChild(emitter)
+                }
+                
+                node.name = ""
+                bombContainer.physicsBody?.isDynamic = false
+                
+                let scaleOut = SKAction.scale(to: 0.001, duration: 0.2)
+                let fadeOut = SKAction.fadeOut(withDuration: 0.2)
+                let group = SKAction.group([scaleOut, fadeOut])
+                
+                let seq = SKAction.sequence([group, .removeFromParent()])
+                bombContainer.run(seq)
+                
+                if let index = activeEnemies.firstIndex(of: bombContainer) {
+                    activeEnemies.remove(at: index)
+                }
+                
+                run(SKAction.playSoundFileNamed("explosiopm", waitForCompletion: <#T##Bool#>))
+                
+            }
+        }
+        
     }
     
     //画面のタッチ開始
